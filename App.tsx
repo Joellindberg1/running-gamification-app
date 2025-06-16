b8f067e1309d72e16b47ef6aaa413ef83b2e6ff4
Joellindberg1 <Joellindberg23@gmail.com>
2025-06-16 14:18:04 +0200

    Setup UserContext  + imports

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { Provider as PaperProvider } from 'react-native-paper';
 import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
+import { UserProvider } from './src/contexts/UserContext';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
